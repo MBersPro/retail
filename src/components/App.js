bf8aea0fe9d9a8718fd64ca77320ace3d3e549d8
Muhammad9500 <83613088+MBersPro@users.noreply.github.com>
2022-03-24 21:45:26 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -48,7 +48,7 @@ const App = () => {
         <Header changePage={changePage} />
         {page.retail && <Retail />}
       </Gradient>
-      <AboutUs/>
+      {page.retail && <AboutUs/>}
       {page.katalog && (
         <Catalog
           addToKorzina={addToKorzina}
