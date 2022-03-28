6c62bdfcfa0cb634061e2e2e7bd3365cd0b9eca2
Muhammad9500 <83613088+MBersPro@users.noreply.github.com>
2022-03-28 16:52:15 +0300

    deleted useState

--- src/components/Korzina/Korzina.js
+++ src/components/Korzina/Korzina.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import styles from "./Korzina.module.css";
 const Korzina = ({ korzina, deleteFromKorzina }) => {
   const onDeleteFromKorzina = (e) => {
