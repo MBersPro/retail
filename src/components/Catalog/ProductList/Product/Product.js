397e58a4e19bb9f88a1fa642b3ae348e01191cea
Muhammad9500 <83613088+MBersPro@users.noreply.github.com>
2022-03-28 16:35:47 +0300

    added Telegram, whatsApp, Number link

--- src/components/Catalog/ProductList/Product/Product.js
+++ src/components/Catalog/ProductList/Product/Product.js
@@ -9,24 +9,26 @@ const Product = ({ product, addToKorzina, korzina, added }) => {
 
   return (
     <li className={styles.li}>
-      <img className={styles.image} src={product.image} alt={product.name} />
-      <p className={styles.p}>{product.name}</p>
-      <button
-        className={
-          added.includes(product.id)
-            ? styles.buttonAdded
-            : styles.buttonNotAdded
-        }
-        type="button"
-        id={product.id}
-        onClick={onAddToKorzina}
-      >
+    <div className={styles.divProduct}>
+        <img className={styles.image} src={product.image} alt={product.name} />
+        <p className={styles.p}>{product.name}</p>
+        <button
+          className={
+            added.includes(product.id)
+              ? styles.buttonAdded
+              : styles.buttonNotAdded
+          }
+          type="button"
+          id={product.id}
+          onClick={onAddToKorzina}
+        >
         {added.includes(product.id) ? (
           <span>Товар добавлен в корзину</span>
         ) : (
           <span>Добавить в корзину</span>
         )}
       </button>
+      </div>
     </li>
   );
 };
--- src/components/Korzina/Korzina.js
+++ src/components/Korzina/Korzina.js
@@ -1,5 +1,9 @@
 import React, {useState} from "react";
 import styles from "./Korzina.module.css";
+import { ReactComponent as Phone } from "./svg/Phone.svg";
+import { ReactComponent as WhatsApp } from "./svg/WhatsApp.svg";
+import { ReactComponent as Telegram } from "./svg/Telegram.svg";
+// import { ReactComponent as LinkedIn } from "./svg/LinkedIn.svg";
 
 const Korzina = ({ korzina, deleteFromKorzina }) => {
   const onDeleteFromKorzina = (e) => {
@@ -8,29 +12,39 @@ const Korzina = ({ korzina, deleteFromKorzina }) => {
   };
   return (
     <main>
-      <div>
-        Для того, чтобы осуществить заказ, свяжитесь с наши менеджером любым
-        удобным для вас способом и назовите ему артукул тех товаров, которые
-        хотите приобрести.
+      <div className={styles.container}>
+        <p className={styles.pInstruction}>
+            Для того, чтобы осуществить заказ,<br/>
+            свяжитесь с наши менеджером люб<br/>
+            ым удобным для вас способом и <br/>
+            назовите ему артукул тех товаров,<br/>
+            которые хотите приобрести.<br/>
+          </p>
+        <div className={styles.divA}>
+        <a href="tg://resolve?domain=buyer_3198" className={styles.aTelegram}><Telegram className={styles.telegramIcon}/>Telegram</a>
+          <a href="https://wa.me/79688588238" className={styles.aWhatsApp}><WhatsApp className={styles.whatsAppIcon}/>WhatsApp</a>
+          <a href="tel:+79688588238" className={styles.aNumber}><Phone className={styles.numberIcon}/>+79688588238</a>
+        </div>
+      
+          <ul className={styles.ul}>
+            {korzina.map((product) => (
+              <li className={styles.li}>
+                <div className={styles.divProduct}>
+                    <img
+                      className={styles.image}
+                      src={product.image}
+                      alt={product.name}
+                    />
+                    <p className={styles.productName}>{product.name}</p>
+                  
+                    <button className={styles.btnDel} type="button" id={product.id} onClick={onDeleteFromKorzina}>
+                      Удалить из корзины
+                    </button>
+                  </div>
+              </li>
+            ))}
+          </ul>
       </div>
-      <a href="tg://resolve?domain=buyer_3198">Telegram</a>
-      <a href="https://wa.me/79688588238">WhatsApp</a>
-      <a href="tel:+79688588238">+79688588238</a>
-      <ul>
-        {korzina.map((product) => (
-          <li>
-            <img
-              className={styles.image}
-              src={product.image}
-              alt={product.name}
-            />
-            <p>{product.name}</p>
-            <button type="button" id={product.id} onClick={onDeleteFromKorzina}>
-              Удалить из корзины
-            </button>
-          </li>
-        ))}
-      </ul>
     </main>
   );
 };
