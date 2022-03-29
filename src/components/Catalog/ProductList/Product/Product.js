74f5f043e644af4912c81774589c4018cf9ff15b
MBers <mbers.pro@gmail.com>
2022-03-29 12:21:33 +0300

    Done

--- src/components/Catalog/ProductList/Product/Product.js
+++ src/components/Catalog/ProductList/Product/Product.js
@@ -9,13 +9,19 @@ const Product = ({ product, addToKorzina, korzina, added }) => {
 
   return (
     <li className={styles.li}>
-      <div>
+      <div className={styles.image_container}>
         <img className={styles.image} src={product.image} alt={product.name} />
       </div>
       <div className={styles.productCharactersContainer}>
         <div className={styles.p_contrainer}>
           <p className={styles.name}>{product.name}</p>
           <p className={styles.price}>{product.price}₽</p>
+          <p className={styles.season}>
+            <span className={styles.season_span}>Сезон:</span>
+            {product.seasons.map((season) => (
+              <span className={styles.season_span}>{season}</span>
+            ))}
+          </p>
         </div>
 
         <button
