02405decfe77a96e9aaa9526f8147ba584a93719
Muhammad9500 <83613088+MBersPro@users.noreply.github.com>
2022-03-30 09:26:00 +0300

    Contacts

--- src/components/Kontakti/Kontakti.js
+++ src/components/Kontakti/Kontakti.js
@@ -3,21 +3,26 @@ import styles from "./Kontakti.module.css";
 
 const Kontakti = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.divPhone}>
-        <a href="tel:+79688588238" className={styles.phone}>
-          Phone: 8-968-858-82-38
-        </a>
-      </div>
-      <div className={styles.divTelegram}>
-        <a
-          href="tg://resolve?domain=retail_manager"
-          className={styles.telegram}
-        >
-          Telegram: RETAIL_MANAGER
-        </a>
-      </div>
-    </div>
+        <div className={styles.container}>
+            <div className={styles.backgroundContacts}>
+                <div className={styles.divContacts}>
+                    <a href="tel:+79688588238" className={styles.phone}>
+                    Номер: +79688588238
+                    </a>
+                    <a
+                    href="tg://resolve?domain=retail_manager"
+                    className={styles.telegram}
+                    >
+                    Telegram: RETAIL_MANAGER
+                    </a>
+                    <a 
+                    href="https://wa.me/79688588238" 
+                    className={styles.WhatsApp}>
+                    WhatsApp: +79688588238
+                    </a>
+                </div>
+            </div>
+        </div>
   );
 };
 
