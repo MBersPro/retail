e15efd1a988becdbe2f64237f254a7855fa661c8
Muhammad9500 <83613088+MBersPro@users.noreply.github.com>
2022-04-19 21:59:34 +0300

    added changes

--- src/components/Catalog/Filter/Filter.js
+++ src/components/Catalog/Filter/Filter.js
@@ -11,27 +11,27 @@ const Filter = ({ setFilter }) => {
     <div className={styles.container}>
       <ul className={styles.ul}>
         <li className={styles.li}>
-          <button type="button" value="all" onClick={onSetFilter}>
+          <button type="button" value="all" onClick={onSetFilter} className={styles.button}>
             Все
           </button>
         </li>
         <li className={styles.li}>
-          <button type="button" value="clothes" onClick={onSetFilter}>
+          <button type="button" value="clothes" onClick={onSetFilter} className={styles.button}>
             Одежда
           </button>
         </li>
         <li className={styles.li}>
-          <button type="button" value="sneakers" onClick={onSetFilter}>
+          <button type="button" value="sneakers" onClick={onSetFilter} className={styles.button}>
             Обувь
           </button>
         </li>
         <li className={styles.li}>
-          <button type="button" value="outerwear" onClick={onSetFilter}>
+          <button type="button" value="outerwear" onClick={onSetFilter} className={styles.button}>
             Верхняя одежда
           </button>
         </li>
         <li className={styles.li}>
-          <button type="button" value="pants" onClick={onSetFilter}>
+          <button type="button" value="pants" onClick={onSetFilter} className={styles.button}>
             Джоггеры
           </button>
         </li>
