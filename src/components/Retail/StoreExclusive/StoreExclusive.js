a32166649898d6806868973521a2b68c87b89950
Muhammad9500 <83613088+MBersPro@users.noreply.github.com>
2022-04-20 09:10:20 +0300

    Done

--- src/components/Retail/StoreExclusive/StoreExclusive.js
+++ src/components/Retail/StoreExclusive/StoreExclusive.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./StoreExclusive.module.css";
+import snikers from "./img/snikers.jpg";
+import top from "./img/Top.jpg";
+import suits from "./img/suits.jpg";
+import jogger from "./img/jogger.jpg";
 const StoreExclusive = () => {
   return (
     <div className={styles.container}>
@@ -7,16 +11,24 @@ const StoreExclusive = () => {
 
       <div className={styles.contdivbox}>
         <div className={styles.divbox1}>
-          <div className={styles.box1}></div>
+          <div className={styles.box1}>
+            <img src={jogger} alt="ДЖОГГЕРЫ" className={styles.image}/>
+          <p className={styles.box1p}>ДЖОГГЕРЫ</p>
+          </div>     
           <div className={styles.box2}>
-            <p className={styles.box2p}>COATS & JACKETS</p>
+            <img className={styles.image} src={top} alt="ВЕРХНЯЯ ОДЕЖДА"/>
+            <p className={styles.box2p}>ВЕРХНЯЯ ОДЕЖДА</p>
           </div>
         </div>
         <div className={styles.divbox2}>
           <div className={styles.box3}>
-            <p className={styles.box3p}>SPORT</p>
+            <img className={styles.image} src={snikers} alt="КРОССОВКИ"/>
+            <p className={styles.box3p}>КРОССОВКИ</p>
           </div>
-          <div className={styles.box4}></div>
+          <div className={styles.box4}>
+            <p>КОСТЮМ</p>
+            <img src={suits} className={styles.image} alt="КОСТЮМ"/>
+          </div>
         </div>
       </div>
     </div>
